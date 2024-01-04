f95f4ee6fde8be2d618e79638cba8d16edf368da
pix303 <303pix@gmail.com>
2024-01-05 00:23:49 +0100

    add(api): axum server

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,29 @@
-use domain::raw_test;
-fn main() {
-    raw_test();
+use axum::{routing::get, Json, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = init_app();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3030").await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .expect("fail to start up server");
+}
+
+fn init_app() -> Router {
+    Router::new()
+        .route("/", get(welcome_handler))
+        .route("/version", get(|| async { "0.0.1" }))
+}
+
+#[derive(serde::Serialize)]
+struct ResponseMessage {
+    code: u8,
+    message: String,
+}
+
+async fn welcome_handler() -> Json<ResponseMessage> {
+    Json(ResponseMessage {
+        code: 0,
+        message: String::from("Welcome to localemgmt API"),
+    })
 }
